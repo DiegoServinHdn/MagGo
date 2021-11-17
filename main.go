089e18fe750b8ffb9d61b2428557fa664ee26d48
Diego Servin <Diegoservinhdn@gmail.com>
2021-11-16 21:51:51 -0600

    Add Icons and buttons

--- main.go
+++ main.go
@@ -1,22 +1,33 @@
 package main
 
 import (
+	// "image/color"
+	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
+	// "fyne.io/fyne/v2/canvas"
 	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/layout"
 	"fyne.io/fyne/v2/widget"
 )
 
 func main() {
-	manGO := app.New()
-	home:= manGO.NewWindow("Mango")
-
-	download := widget.NewLabel("Hello ManGo")
-	home.SetContent(container.NewVBox(
-		download,
+	manGo:= app.New()
+	icon, _ := fyne.LoadResourceFromPath("icons\\mangologo.png")
+	myWindow := manGo.NewWindow("ManGo")
+	myWindow.Resize(fyne.NewSize(500, 500))
+	Welcome := widget.NewLabel("Welcome to ManGO")
+	centered_welcome := container.New(layout.NewHBoxLayout(), layout.NewSpacer(), Welcome, layout.NewSpacer())
+	buttons := container.New(layout.NewHBoxLayout(), 
+		layout.NewSpacer(), 
 		widget.NewButton("Descargar", func() {
-			download.SetText("Welcome :)")
+			Welcome.SetText("hi")
 		}),
-	))
+		widget.NewButton("Cerrar", func ()  {
+			myWindow.Close()
+		}), 
+		layout.NewSpacer())
+	myWindow.SetContent(container.New(layout.NewVBoxLayout() ,centered_welcome, buttons))
 
-	home.ShowAndRun()
+	myWindow.SetIcon(icon)
+	myWindow.ShowAndRun()
 }
